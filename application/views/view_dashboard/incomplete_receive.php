8deac8ddb5e4226eca568ddd2dd6a5834259bf0c
boyhaqi99 <boy.haqi@iforce.co.id>
2023-11-29 09:03:04 +0700

    Penambahan fitur download excel di Dashboard Incompletes PO

--- application/views/view_dashboard/incomplete_receive.php
+++ application/views/view_dashboard/incomplete_receive.php
@@ -40,9 +40,43 @@
 <script>
 	var base_url			= '<?php echo base_url(); ?>';
 	//var active_controller	= 'Dashboard';
+
+	const today = new Date();
+	const yyyy = today.getFullYear();
+	let mm = today.getMonth() + 1; // Months start at 0!
+	let dd = today.getDate();
+
+	//if (dd < 10) dd = '0' + dd;
+	if (mm < 10) mm = '0' + mm;
+
+	const formattedToday = mm + '-' + yyyy;
+
 	$(document).ready(function(){
-		$('#table_late').dataTable();
-		
-		
-	})
+		$('#table_late').dataTable({
+			order			: [],
+			lengthMenu		: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+			iDisplayLength	: 10,
+			dom				: 'Bfrtip', 
+			buttons			: [
+							{
+								extend: 'pageLength',
+								text:      '<i class="fa fa-list-ol"></i> <b>Show</b>',
+								className: "Btntable"
+							},
+							
+							{
+								extend: 'excelHtml5',
+								text:      '<i class="fa fa-file-excel-o"></i> <b>Download Excel</b>',
+								titleAttr: 'Excel',
+								className: "Btntable",
+								title: 'Dashboard Incomplete - Data Receive Bulan '+formattedToday,
+								messageTop: 'Report Data Receive',
+								exportOptions: {
+								columns: [0,1,2,3,4,5]
+								}
+							},
+
+						],
+		});
+	});
 </script>
