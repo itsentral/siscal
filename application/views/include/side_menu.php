e9eb90862c7fc719d729db1250d321bc1c24cb2c
boyhaqi99 <boy.haqi@iforce.co.id>
2024-02-13 14:57:38 +0700

    Perubahan template

--- application/views/include/side_menu.php
+++ application/views/include/side_menu.php
@@ -44,46 +44,189 @@
 		.chosen-container {
 			width: 100% !important;
 		}
+
+		/* Start Custom Boy */
+		.navbar{
+			border-radius: 0px 0px 30px 0px !important;
+			background: rgba(255, 255, 255, 0.1) !important;
+			backdrop-filter: blur(15px) !important;
+			box-shadow: 0 15px 25px rgba(0, 0, 0, 0.1) !important;
+		}
+		.logo{
+			background-color: #2F92E4 !important;
+			color: white !important;
+			border-radius: 0px 0px 17px 17px !important;
+		}
+		.btn-logout-cs{
+			align: center !important;
+			background-color: #d9534f !important;
+			color: white !important;
+			font-size: 13.3px !important;
+			padding: 8px !important;
+			margin-top: 6px !important;
+			border-radius: 20px !important;
+			width: 90px;
+			border: none !important;
+			box-shadow: 0 1px 2px rgba(0,0,0,0.07), 
+					0 2px 4px rgba(0,0,0,0.07), 
+					0 4px 8px rgba(0,0,0,0.07), 
+					0 8px 16px rgba(0,0,0,0.07),
+					0 16px 32px rgba(0,0,0,0.07), 
+					0 32px 64px rgba(0,0,0,0.07);
+		}
+		a.btn-logout-cs:hover{
+			color: white;
+			transition: all 150ms linear;
+			opacity: .85;
+		}
+		.sidebar-toggle{
+			margin-left: 25px !important;
+			color:black !important;
+		}
+		.sidebar-toggle:hover{
+			background-color: rgba(255, 255, 255, 0.1) !important;
+		}
+
+		/* .foocus {
+			background: #8BC2A1;
+			border-radius: 7.5px;
+			width: 168px;
+			height: 38px;
+			font-weight: 700 !important;
+		} */
+		
+		.sidebar-menu li.active{
+			border-right: 3.2px solid #2F92E4 !important;
+			color: #000;
+		}
+
+		.menu-0 li.active {
+			color: #000;
+			border-right: 3.5px solid #2F92E4 !important;
+		}
+
+		.menu-1 li.active {
+			color: #000;
+			border-right: 4.5px solid #2F92E4 !important;
+		}
+
+		@media (max-width: 48em) {
+			.logo{
+				border-radius: 0px 0px 0px 0px !important;
+			}
+			.navbar{
+				border-radius: 0px 0px 35px 35px !important;
+			}
+			.logo-lg{
+				width: 30%;
+			}
+		}
+		@media (max-width: 768px) {
+			.logo-lg{
+				width: 30%;
+			}
+		}
+		@media only screen and (max-width: 600px) {
+			.logo-lg{
+				width: 40% !important;
+			}
+		}
+		@media only screen and (max-width: 530px) {
+			.logo-lg{
+				width: 48% !important;
+			}
+		}
+		@media only screen and (max-width: 360px) {
+			.logo-lg{
+				width: 60% !important;
+			}
+		}
+		/* End Custom Boy */
+
 	</style>
 </head>
 
-<body class="hold-transition skin-blue sidebar-mini sidebar-collapse">
+<body class="skin-blue-light fixed sidebar-mini sidebar-mini-expand-feature">
 	<div class="wrapper">
 		<header class="main-header">
 			<a href="#" class="logo">
-				<span class="logo-mini"><b>SCS</b></span>
-
+				<img src="<?php echo base_url('assets/img/sc_logo.png') ?>" class="logo-mini" alt="User Image" width="250%">
+				<img src="<?php echo base_url('assets/img/logo_flat_white.png') ?>" class="logo-lg" alt="User Image" width="95%">
 			</a>
 			<nav class="navbar navbar-static-top">
 				<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
 					<span class="sr-only">Toggle navigation</span>
 				</a>
-				<a class="navbar-brand" href="#"><b>KALIBRASI DASHBOARD</b></a>
 				<div class="navbar-custom-menu">
-					<ul class="nav navbar-nav">
-						<li class="dropdown tasks-menu">
-							<a href="<?php echo base_url(); ?>index.php/dashboard/logout" data-role="qtip" title="Sign Out">
-								<i class="fa fa-sign-out"></i>
-
+					<ul class="nav navbar-nav" style="margin-right: 30px;">
+						<li>
+							<a href="<?php echo base_url(); ?>dashboard/logout" class="btn-logout-cs" data-role="qtip">
+							<text style="margin-left:10px;"><i class="fa fa-power-off"></i> Logout</text>
 							</a>
 						</li>
-						<li class="dropdown user user-menu">
+						<!-- <li class="dropdown user user-menu">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 								<img src="<?php echo base_url('assets/img/avatar.png') ?>" class="img-circle" alt="User Image" height="20px" width="20px">
-								<span class="hidden-xs"> <?php echo $this->session->userdata('siscal_username'); ?></span>
+								<span class="hidden-xs"> <text style="color:black;font-weight: bold;"><?php echo $this->session->userdata('siscal_username'); ?></text></span>
 							</a>
-						</li>
+						</li> -->
 					</ul>
 				</div>
 			</nav>
 		</header>
 		<aside class="main-sidebar">
 			<section class="sidebar">
+				<div class="user-panel">
+					<div class="pull-left image">
+						<img src="<?php echo base_url('assets/img/avatar.png') ?>" class="img-circle" alt="User Image">
+					</div>
+					<div class="pull-left info">
+						<p><?php echo $this->session->userdata('siscal_username'); ?></p>
+						<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+					</div>
+				</div>
 				<?php
 				$Menus	= group_menus_access();
 				//echo"<pre>";print_r($Menus);
 				render_left_menus($Menus);
 				?>
+
+
+				<script>
+					// $(document).ready(function() {
+					// 	$('.sidebar-menu > li').click(function() {
+					// 		let a = localStorage.setItem("menu1", $(this).attr("id"));
+					// 		$('li').removeClass("active");
+					// 		$(this).addClass("active");
+					// 		console.log('getMenu:' + $(this).attr("id"));
+					// 	});
+
+					// 	$('.menu-0 > li').click(function() {
+					// 		let a = localStorage.setItem("menu2", $(this).attr("id"));
+					// 		$('li').removeClass("active");
+					// 		$(this).addClass("active");
+					// 		console.log('getMenu2:' + $(this).attr("id"));
+					// 	});
+
+					// 	$('.menu-1 > li').click(function() {
+					// 		let a = localStorage.setItem("menu3", $(this).attr("id"));
+					// 		$('li').removeClass("active");
+					// 		$(this).addClass("active");
+					// 		console.log('getMenu3:' + $(this).attr("id"));
+					// 	});
+						
+					// 	var ActiveSet = window.localStorage.getItem("menu1");
+					// 		ActiveSet ? $("#" + ActiveSet).addClass("active") : $('#1-1').addClass("active");
+
+					// 	var ActiveSet2 = window.localStorage.getItem("menu2");
+					// 		ActiveSet2 ? $("#" + ActiveSet2).addClass("active") : $('#7-1').addClass("active");
+
+					// 	var ActiveSet3 = window.localStorage.getItem("menu3");
+					// 		ActiveSet3 ? $("#" + ActiveSet3).addClass("active") : $('#8-1').addClass("active");
+					// });
+				</script>
+
+
 
 				<!--
 				<ul class="sidebar-menu">
@@ -124,7 +267,7 @@
 				<ol class="breadcrumb">
 					<li><?php echo ucwords(str_replace('_', ' ', strtolower($this->uri->segment(1)))); ?></a></li>
 					<li class="active">
-						<a href="<?php echo base_url() . 'index.php/' . strtolower($this->uri->segment(1) . '/' . $action); ?>">
+						<a href="<?php echo base_url(). strtolower($this->uri->segment(1) . '/' . $action); ?>">
 							<?php echo ucwords(strtolower($action)); ?>
 						</a>
 					</li>
